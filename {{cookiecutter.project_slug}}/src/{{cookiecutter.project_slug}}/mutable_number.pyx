878c65dd6ed55caadb90d262494901e11c8751f4
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-02-07 17:31:57 +0100

    convert more of MutableClass to Cython

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/mutable_number.pyx
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/mutable_number.pyx
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """A class representing a mutable number."""
+import cython
 
 
 cdef class MutableNumber:
@@ -7,29 +8,25 @@ cdef class MutableNumber:
     def __init__(self, number):
         self.history = [number]
 
-    def get(self, idx=-1):
+    cpdef get(self, int idx=-1):
         return self.history[idx]
 
-    def add(self, addend):
-        number = self.get()
-        number += addend
+    cpdef MutableNumber add(self, cython.numeric addend):
+        cdef cython.numeric number = self.get() + addend
         self.history.append(number)
         return self
 
-    def subtract(self, subtrahend):
-        number = self.get()
-        number -= subtrahend
+    cpdef MutableNumber subtract(self, cython.numeric subtrahend):
+        cdef cython.numeric number = self.get() - subtrahend
         self.history.append(number)
         return self
 
-    def multiply(self, factor):
-        number = self.get()
-        number *= factor
+    cpdef MutableNumber multiply(self, cython.numeric factor):
+        cdef cython.numeric number = self.get() * factor
         self.history.append(number)
         return self
 
-    def divide(self, divisor):
-        number = self.get()
-        number /= divisor
+    cpdef MutableNumber divide(self, cython.numeric divisor):
+        cdef cython.numeric number = self.get() / divisor
         self.history.append(number)
         return self
