5776a227f3937c58214642b8233f3ed70d3954ab
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-02-07 14:52:55 +0100

    turn MutableNumber into extension type
    enable cython compilation

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/mutable_number.pxd
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+cdef class MutableNumber:
+    cdef list history
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/mutable_number.pyx
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/mutable_number.pyx
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Main module."""
+"""A class representing a mutable number."""
 
 
-class MutableNumber:
+cdef class MutableNumber:
+
     def __init__(self, number):
         self.history = [number]
 
@@ -10,21 +11,25 @@ class MutableNumber:
         return self.history[idx]
 
     def add(self, addend):
-        number = self.get() + addend
+        number = self.get()
+        number += addend
         self.history.append(number)
         return self
 
     def subtract(self, subtrahend):
-        number = self.get() - subtrahend
+        number = self.get()
+        number -= subtrahend
         self.history.append(number)
         return self
 
     def multiply(self, factor):
-        number = self.get() * factor
+        number = self.get()
+        number *= factor
         self.history.append(number)
         return self
 
     def divide(self, divisor):
-        number = self.get() / divisor
+        number = self.get()
+        number /= divisor
         self.history.append(number)
         return self
